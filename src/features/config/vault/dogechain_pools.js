10fa0e1b2b9116ee011c6c65cafef9ce1d12b19d
autobarkfinance <111478926+autobarkfinance@users.noreply.github.com>
2022-08-18 18:22:55 +0900

    Update dogechain_pools.js

--- src/features/config/vault/dogechain_pools.js
+++ src/features/config/vault/dogechain_pools.js
@@ -1,4 +1,29 @@
 export const dogechainPools = [
-    
+  {
+    id: 'yodeswap-usdc-usdt',
+    name: 'Yodeswap USDC USDT',
+    token: 'USDC-USDT yodeLP',
+    tokenDescription: 'Yodeswap',
+    tokenAddress: '0x0346DB3b5bAB769877738E7ED1C3b33e2b044F5c',
+    tokenDecimals: 18,
+    tokenDescriptionUrl: '#',
+    earnedToken: 'bVAULT-USDC-USDT-yodeswap',
+    earnedTokenAddress: '0x765277EebeCA2e31912C9946eAe1021199B39C61',
+    earnContractAddress: '0x765277EebeCA2e31912C9946eAe1021199B39C61',
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: 'lps',
+    oracleId: 'yodeswap-usdc-usdt',
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: 'active',
+    platform: 'Stellaswap',
+    assets: ['USDC', 'USDT'],
+    risks: ['COMPLEXITY_LOW', 'NEW_STRAT', 'IL_LOW', 'MCAP_LARGE', 'AUDIT', 'CONTRACTS_VERIFIED'],
+    stratType: 'StratMultiLP',
+    withdrawalFee: '0%',
+    addLiquidityUrl:
+      'https://yodeswap.dog/zap?poolAddress=0xD95b66FBD36C5754FE7C837500cccBf113fD6ac6&currencyId=0xcd3B51D98478D53F4515A306bE565c6EebeF1D58',
+  },
   ];
- 
+  
